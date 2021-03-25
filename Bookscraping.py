7f4a0696862453ff2e256d638a4ce3c386e63b72
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2021-03-25 16:45:17 +0100

    Books_count_bycategory, books_url_by_category, products_images_url and download_products_images functions added.

--- Bookscraping.py
+++ Bookscraping.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib
 
+#Retrieve requested data from a specific book
 def book_data(url):
 
     r = requests.get(url)
@@ -33,16 +35,28 @@ def book_data(url):
 #     extraction.write("product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category,review_rating,image_url\n")
 #     extraction.write(product_page_url + "," + universal_product_code + "," + title.replace(",","") + "," + price_including_tax + "," + price_excluding_tax + "," + number_available + "," + product_description.replace(",","") + "," + category + "," + review_rating + "," + image_url)
 
-
+#TODO Retrieve the list of books categories from the website first as a list then as a dictionary
 def categories_list(url):
     r = requests.get(url)
     if r.status_code != 200:
         print("Site non disponible. Merci de réessayer plus tard.")
     soup = BeautifulSoup(r.text, features="html.parser")
     categories = soup.find("div",class_="side_categories").find("ul",class_="nav nav-list").text.replace(" ","").split("\n")
-    i = 0
-    return [category for category in categories if category != ""]
+    categories_list = []
+    i = 1
+    for category in categories:
+        if category != "":
+            categories_list.append([i,category.lower()])
+            i += 1
+        else:
+            continue
+    categories_dictionary = {}
+    for category in categories_list:
+        categories_dictionary[category[1]] = category[0]
+    return categories_dictionary
+print(categories_list("http://books.toscrape.com/index.html"))
 
+#Retrieve the total number of categories
 def categories_count(url):
     r = requests.get(url)
     if r.status_code != 200:
@@ -62,17 +76,21 @@ def categories_count(url):
 #
 # print(categories_count("http://books.toscrape.com/index.html"))
 
+#TODO Retrieve total count of books for a specific category
 def books_count_by_category(category):
-    url = "http://books.toscrape.com/catalogue/category/books/"+category+"_31/index.html"
+    category = category.lower()
+    url = "http://books.toscrape.com/catalogue/category/books/"+category+"_"+str(categories_list("http://books.toscrape.com/index.html")[category])+"/index.html"
     r = requests.get(url)
     soup = BeautifulSoup(r.text,features="html.parser")
     books_count = soup.find("form",class_="form-horizontal").find("strong").text
     return books_count
 
-print(books_count_by_category("horror"))
+print(books_count_by_category("Romance"))
 
+#Retrieve all books urls belonging to a specific category
 def books_url_by_category(category):
-    url = "http://books.toscrape.com/catalogue/category/books/"+category+"_31/index.html"
+    category = category.lower()
+    url = "http://books.toscrape.com/catalogue/category/books/"+category+"_"+str(categories_list("http://books.toscrape.com/index.html")[category])+"/index.html"
     r = requests.get(url)
     soup = BeautifulSoup(r.text,features="html.parser")
     books_url = soup.find("ol",class_="row")
@@ -81,10 +99,24 @@ def books_url_by_category(category):
     fin = books_url.find(".html")
     return "http://books.toscrape.com/catalogue"+books_url[debut+8:fin+5]
 
-print(books_url_by_category("horror"))
+print(books_url_by_category("Romance"))
 
-def download_products_images():
-    pass
+#Retrieve product image url from product page
+def products_images_url(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text,features="html.parser")
+    image_url = soup.find("div",class_="item active").find("img")["src"].replace("../..","http://books.toscrape.com")
+    return image_url
+
+print(products_images_url("http://books.toscrape.com/catalogue/pet-sematary_726/index.html"))
+
+#Download product image from website to the working directory
+def download_products_images(url):
+    urllib.request.urlretrieve(url,url.split("/")[-1])
+    return
+
+download_products_images("http://books.toscrape.com/media/cache/99/ef/99ef2f3987ee016bdd120a4579ef98f5.jpg")
+
 
 
 # import time
